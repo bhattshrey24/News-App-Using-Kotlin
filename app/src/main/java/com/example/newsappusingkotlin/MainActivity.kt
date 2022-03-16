202e0d00a61b30f6324429d651d2953daf647f25
Shrey Bhatt <bhatt.shrey1224@gmail.com>
2022-03-16 12:28:14 +0530

    Added bottom nav

--- app/src/main/java/com/example/newsappusingkotlin/MainActivity.kt
+++ app/src/main/java/com/example/newsappusingkotlin/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.newsappusingkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+  //Todo
+  // add splash screen
+  // add firebase login functionality
+  // add select language preference feature
+  // add bottom navigation
+  // add room
+  // add retrofit
+  // add theme colors
+  // add push notification feature giving notification about of daily news
+  // add drawer
+  // add search feature
+  // add "share with" feature
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myBottomNav=findViewById<BottomNavigationView>(R.id.my_bottom_nav)
+        val navController=findNavController(R.id.my_nav_host_fragment)
+        myBottomNav.setupWithNavController(navController)
     }
 }
