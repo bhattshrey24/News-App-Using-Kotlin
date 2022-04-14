68fbdf97f57f4e2c3ff9542f8c59780990fd2db0
Shrey Bhatt <bhatt.shrey1224@gmail.com>
2022-04-14 13:00:14 +0530

    todos mentioned

--- app/src/main/java/com/example/newsappusingkotlin/MainActivity.kt
+++ app/src/main/java/com/example/newsappusingkotlin/MainActivity.kt
@@ -16,40 +16,31 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
     //Todo
     // add select language preference feature
-    // add room
+    // add search feature
     // add retrofit
     // add theme colors
     // add push notification feature giving notification about of daily news
     // add "share with" feature
-    // Current:-
-    // add recycler view
-    // make fetching of random news from newsApi using retrofit
-    // add drawer
-    // add search feature
     // Authentication :-
     //  add create new user functionality
     //  add Custom Login
     //  add google and facebook login
     //  add forgot password functionality
+    // CURRENT:-
+    //  Fix app bar , add menu and icons in it fix Its UI
+    //  add drawer
+    //  add room ie. cache your news articles , user details etc
+    //  add user functionality ie. user data stored in firebase having its username , interests , saved/bookmarked news articles
+
 
     private val binding: ActivityMainBinding by lazy {//this is lazy initialization
         ActivityMainBinding.inflate(layoutInflater, null, false)
     }
-    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-//        val repository = MyRepository()
-//        val viewModelFactory = MainViewModelFactory(repository)
-//
-//        viewModel= ViewModelProvider(this,viewModelFactory).get(MainViewModel::class.java)
-//        viewModel.getPost("in") // in is the code for "India"
-//
-//        viewModel.myResponse.observe(this, Observer { response->
-//            Log.d("Response " , "Response is : $response")
-//        })
         val myBottomNav = findViewById<BottomNavigationView>(R.id.my_bottom_nav)
         val navController = findNavController(R.id.my_nav_host_fragment)
         myBottomNav.setupWithNavController(navController)
